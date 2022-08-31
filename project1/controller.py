c3284b5bbe044b8b4a17222cb9e97348920a9c13
James Halladay <54321627+jehalladay@users.noreply.github.com>
2022-08-31 13:24:42 -0600

    updating readme to include project 1

---
+++ project1/controller.py
@@ -0,0 +1,20 @@
+'''
+Assignment - Project 1: Hangman
+File: controller.py
+Description: This program will control the game and manages passing the game state
+    to the view in display.py.
+James Halladay
+Advanced Programming with Python
+Date: 8/29/2022
+
+
+*******************************************************************************
+Here we are building a hangman game.  The game will use a MVC design pattern 
+    where the model is in ./game.py, the view is in ./display.py, and this file
+    is the controller.
+
+
+
+
+*******************************************************************************
+'''
--- project1/test_game.py
+++ project1/test_game.py
@@ -1,3 +1,17 @@
+'''
+Assignment - Project 1: Hangman
+File: test_game.py
+Description: This program will run tests on game.py
+James Halladay
+Advanced Programming with Python
+Date: 8/29/2022
+
+
+*******************************************************************************
+
+*******************************************************************************
+'''
+
 import pytest
 
 from game import Game
